d0f9c729c3f0297058ea0bb9fc9a42105fc85891
Preeti Ahuja <preetia@codeaurora.org>
2015-03-13 16:00:58 -0700

    Base: Handle NPE in TelephonyManager
    ITelphony service instance could be null at boot up
    when device activation is taking place.
    
    An additional NPE is handled here compared to this commit's original
    form (If72622a2d22a20b1ce909134ce4c6446b132dffe):
      NPE exception handling in getDataEnabled has been backported from:
      GpsLocationProvider: Read data enabled status from TelephonyManager.
      I0333050d2a5b8d484466e1b1914dcfb9965398c9
    
      This addresses BUGDUMP-1125891
    
    Change-Id: I89468a2b1cf680acb1edfa27ad1ffbfb145caede
    (cherry picked from commit 05d1b23d509b6a2c656b450702a287790b640bf7)
    (cherry picked from commit 0d317e751b58a31d883376fc947d04eb0b8d2b57)
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	telephony/java/android/telephony/TelephonyManager.java
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -4233,7 +4233,9 @@ public class TelephonyManager {
             if (telephony != null)
                 return telephony.needsOtaServiceProvisioning();
         } catch (RemoteException e) {
-            Log.e(TAG, "Error calling ITelephony#needsOtaServiceProvisioning", e);
+            Log.e(TAG, "Error calling ITelephony#isDataPossibleForSubscription", e);
+        } catch (NullPointerException npe) {
+            Log.e(TAG, "Error calling ITelephony#isDataPossibleForSubscription", npe);
         }
         return false;
     }
@@ -4260,7 +4262,9 @@ public class TelephonyManager {
             if (telephony != null)
                 telephony.setDataEnabled(subId, enable);
         } catch (RemoteException e) {
-            Log.e(TAG, "Error calling setDataEnabled", e);
+            Log.e(TAG, "Error calling ITelephony#needsOtaServiceProvisioning", e);
+        } catch (NullPointerException npe) {
+            Log.e(TAG, "Error calling ITelephony#needsOtaServiceProvisioning", npe);
         }
     }
 
@@ -4327,7 +4331,8 @@ public class TelephonyManager {
             if (telephony != null)
                 return telephony.isVideoCallingEnabled(getOpPackageName());
         } catch (RemoteException e) {
-            Log.e(TAG, "Error calling ITelephony#isVideoCallingEnabled", e);
+            Log.e(TAG, "Error calling ITelephony#getDataEnabled", e);
+        } catch (NullPointerException e) {
         }
         return false;
     }
